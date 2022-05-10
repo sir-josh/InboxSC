0bf02dc783f6fda6fde0fa0c5adeb703d6e4a24a
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2022-05-10 22:06:34 +0100

    code: export compile script module

--- compile.js
+++ compile.js
@@ -6,4 +6,4 @@ const solc = require('solc');
 const inboxContractPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
 const source = fs.readFileSync(inboxContractPath, 'utf-8');
 
-console.log(solc.compile(source, 1));
+module.exports = solc.compile(source, 1).contracts[':Inbox'];
