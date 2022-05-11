45f3f765d2028f156314de76bf2fc94904975945
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2022-05-11 09:15:15 +0100

    code: fetch accounts from ganache

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,3 +3,18 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+
+let fetchedAccounts;
+
+beforeEach(async () => {
+    //Get a list of all accounts
+    fetchedAccounts = await web3.eth.getAccounts();
+
+    //Use one of the accounts to deploy a contract 
+});
+
+describe('Inbox Class', () => {
+    it('deploys a contract', ()=>{
+        console.log(fetchedAccounts);
+    });
+});
