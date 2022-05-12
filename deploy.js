f3b6047241fb7fbee9b33d8838e9eb638d593fad
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2022-05-12 10:02:45 +0100

    code: Wallet provider setup

--- deploy.js
+++ deploy.js
@@ -1,1 +1,11 @@
 // deploy code will go here
+const HDWallectProvider = require('@truffle/hdwallet-provider');
+const Web3 =  require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = HDWallectProvider(
+    process.env.PROVIDERKEY,
+    'https://rinkeby.infura.io/v3/dad984162b06451eb4babfcc418e113c'
+);
+
+const web3 = new Web3(provider);
